a307566a6e5eba880de5ec75731f5362b7c1a00c
gorrec41 <66937048+gorrec41@users.noreply.github.com>
2021-01-20 20:35:49 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     const navbtn = document.querySelector('.btn')
     const modal = document.querySelector('.modalka')
-    const acc = document.getElementsByClassName("four_accordion");
+    const acc = document.getElementsByClassName("four_accordion")
     modal.classList.remove('activ')
     navbtn.addEventListener('click', function(){
-        modal.classList.toggle('activ');
+        modal.classList.toggle('activ')
     })
 
 
     function onIn() {
         acc.mouseover(function () {
-            this.classList.mouseover("active_accordion");
-            console.log('ffff');
+            this.classList.mouseover("active_accordion")
+            console.log('ffff')
         })
     }
 
@@ -19,14 +19,15 @@ $(document).ready(function(){
 
     for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
-        this.classList.toggle("active_accordion");
+        this.classList.toggle("active_accordion")
         const panel = this.nextElementSibling;
         if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
+            panel.style.display = "none"
         }
-    });
+        else {
+            panel.style.display = "block"
+        }
+    })
     }
     $('.owl-carousel').owlCarousel({
         loop:true,
